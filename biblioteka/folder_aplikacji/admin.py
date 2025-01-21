c4d6c8b2714e931572125d872d45f2b111d30e86
jkozdrowicz <jkozdrowicz1@st.swps.edu.pl>
2025-01-21 11:43:23 +0100

    Zrobione wstępne models.py i admin.py

--- biblioteka/folder_aplikacji/admin.py
+++ biblioteka/folder_aplikacji/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Kategoria, Autor, Ksiazka, Czytelnik, Wypozyczenie
 
-# Register your models here.
+admin.site.register(Kategoria)
+admin.site.register(Ksiazka)
+admin.site.register(Autor)
+admin.site.register(Czytelnik)
+admin.site.register(Wypozyczenie)
--- biblioteka/folder_aplikacji/models.py
+++ biblioteka/folder_aplikacji/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Statyczne listy wyboru
+JEZYKI = models.TextChoices('Języki', 'Polski Angielski Niemiecki Francuski Hiszpański')
+
+STATUS_KSIAZKI = (
+    ('D', 'Dostępna'),
+    ('Z', 'Zarezerwowana'),
+    ('W', 'Wypożyczona'),
+)
+
+class Kategoria(models.Model):
+    nazwa = models.CharField(max_length=50, unique=True)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Autor(models.Model):
+    imie = models.CharField(max_length=40)
+    nazwisko = models.CharField(max_length=60)
+    data_urodzenia = models.DateField(null=True, blank=True)
+    narodowosc = models.CharField(max_length=50, choices=JEZYKI.choices, default=JEZYKI.Polski)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+class Ksiazka(models.Model):
+    tytul = models.CharField(max_length=120)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    kategoria = models.ForeignKey(Kategoria, null=True, blank=True, on_delete=models.SET_NULL)
+    data_publikacji = models.DateField(null=True, blank=True)
+    jezyk = models.CharField(max_length=20, choices=JEZYKI.choices, default=JEZYKI.Angielski)
+    status = models.CharField(max_length=1, choices=STATUS_KSIAZKI, default=STATUS_KSIAZKI[0][0])
+
+    def __str__(self):
+        return f"{self.tytul} by {self.autor}"
+
+class Czytelnik(models.Model):
+    uzytkownik = models.OneToOneField(User, on_delete=models.CASCADE)
+    wypozyczone_ksiazki = models.ManyToManyField(Ksiazka, blank=True, through='Wypozyczenie')
+
+    def __str__(self):
+        return self.uzytkownik.username
+
+class Wypozyczenie(models.Model):
+    czytelnik = models.ForeignKey(Czytelnik, on_delete=models.CASCADE)
+    ksiazka = models.ForeignKey(Ksiazka, on_delete=models.CASCADE)
+    data_wypozyczenia = models.DateField(default=date.today)
+    data_zwrotu = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.czytelnik} wypożyczył(a) {self.ksiazka.tytul} dnia {self.data_wypozyczenia}"
+
+
+
